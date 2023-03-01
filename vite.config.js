6d377ee395963980743d37eb7fc850793b7c3c81
Nagham Al Halabi <nagham@kamkalima.com>
2023-03-01 15:49:38 +0200

    add base for the config vite file so that it get's hosted on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 export default {
   entry: './dist/views/index.html',
     server: {
+      base: '/oh-studio/',
       index: './dist/views/index.html',
       open: './dist/views/index.html'
     }
