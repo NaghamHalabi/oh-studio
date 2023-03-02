ee7dd872f92a3fe5da4c0fe13d695de04c3c4f0c
Nagham Al Halabi <nagham@kamkalima.com>
2023-03-02 20:41:02 +0200

    update base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 export default {
   entry: './dist/views/home.html',
     server: {
-      base: '/oh-studio/',
+      base: '/oh-studio/views/home',
       index: './dist/views/home.html',
       open: './dist/views/home.html',
     }
