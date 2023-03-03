5f568185b36e835f107de72dd173931b9417b231
Nagham Al Halabi <nagham@kamkalima.com>
2023-03-03 15:39:53 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-//clear vite
-// export default {
-//   entry: './dist/views/home.html',
-//     server: {
-//       base: '/oh-studio/views/git ',
-//       index: './dist/views/home.html',
-//       open: './dist/views/home.html',
-//     }
-//   }
+export default {
+  entry: './dist/views/home.html',
+    server: {
+      base: '/oh-studio/views/git ',
+      index: './dist/views/home.html',
+      open: './dist/views/home.html',
+    }
+  }
