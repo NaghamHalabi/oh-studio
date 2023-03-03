ff45bbf8da28e6c35d753d3cee3a6f59b1e9f428
NaghamHalabi <32820558+NaghamHalabi@users.noreply.github.com>
2023-03-03 10:38:40 +0200

    add vite config to the subtree

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+//clear vite
+// export default {
+//   entry: './dist/views/home.html',
+//     server: {
+//       base: '/oh-studio/views/git ',
+//       index: './dist/views/home.html',
+//       open: './dist/views/home.html',
+//     }
+//   }
