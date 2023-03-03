29d009678d34878e6d9c4af275bf0bf9652a0f2a
NaghamHalabi <32820558+NaghamHalabi@users.noreply.github.com>
2023-03-03 15:40:37 +0200

    remove vite config

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-//clear vite
-// export default {
-//   entry: './dist/views/home.html',
-//     server: {
-//       base: '/oh-studio/views/git ',
-//       index: './dist/views/home.html',
-//       open: './dist/views/home.html',
-//     }
-//   }
